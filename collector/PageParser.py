1303d9b3f6943f0bd1d700dbdbd89b7544321a58
Dan <x1500491@student.ncirl.ie>
2018-08-07 21:17:41 +0100

    DM and EM Methods

--- collector/PageParser.py
+++ collector/PageParser.py
@@ -14,7 +14,64 @@ class Parser:
         self.game_results = dict()
         self.db = Database()
 
-    def add_lotto(self):
+    def add_daily_millions(self, single_game=True):
+        data_to_insert = []
+        for game_counter, game in enumerate(self.game_results['daily-million']):
+            for i, result in enumerate(game['results']):
+                game_data = []
+                if i == 0:
+                    game_data.append("Daily Million")
+                if i == 1:
+                    game_data.append("Daily Million Plus")
+
+                game_data.append(game['date'])
+
+                game_data = game_data + result
+
+                data_to_insert.append(tuple(game_data))
+
+            if single_game is True:
+                break
+
+        print("Inserting Daily Millions Results to DB")
+        self.db.alter_data(
+            "INSERT INTO lottery.daily_millions(game_type, game_date, number_1, number_2, number_3, number_4, number_5, number_6, bonus_number)"
+            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", data_to_insert
+        )
+
+
+    def add_euromillions(self, single_game=True):
+        eur_mil = []
+        eur_mil_plus = []
+        for game_counter, game in enumerate(self.game_results['euromillions']):
+            for i, result in enumerate(game['results']):
+                game_data = []
+                if i == 0:
+                    game_data.append("Euromillions")
+                    game_data.append(game['date'])
+                    game_data = game_data + result
+                    eur_mil.append(tuple(game_data))
+                if i == 1:
+                    game_data.append("Euromillions Plus")
+                    game_data.append(game['date'])
+                    game_data = game_data + result
+                    eur_mil_plus.append(tuple(game_data))
+
+            if single_game is True:
+                break
+
+        print("Inserting Euromillions Results to DB")
+        self.db.alter_data(
+            "INSERT INTO lottery.euro_millions(game_type, game_date, number_1, number_2, number_3, number_4, number_5, "
+            "lucky_star_1, lucky_star_2) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);", eur_mil
+        )
+        print("Inserting Euromillions Plus Results to DB")
+        self.db.alter_data(
+            "INSERT INTO lottery.euro_millions(game_type, game_date, number_1, number_2, number_3, number_4, number_5) "
+            " VALUES (%s, %s, %s, %s, %s, %s, %s);", eur_mil_plus
+        )
+
+    def add_lotto(self, single_game=True):
         data_to_insert = []
         for game in self.game_results['lotto']:
             for i, result in enumerate(game['results']):
@@ -31,12 +88,14 @@ class Parser:
 
                 data_to_insert.append(tuple(game_data))
 
+            if single_game is True:
+                break
+
         print("Inserting Lotto Results to DB")
         self.db.alter_data(
             "INSERT INTO lottery.lotto(game_type, game_date, number_1, number_2, number_3, number_4, number_5, number_6, bonus_number)"
             "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", data_to_insert
         )
-
 
     """
         Method generates all data by date and sets game_results with each results.
@@ -66,6 +125,8 @@ class Parser:
                 game_date = datetime.strptime(game_date[4:], '%d %B %Y, %H:%M%p')
             else:
                 game_date = datetime.strptime(game_date[4:], '%d %B %Y').date()
+
+            #print(game_date)
 
             data['date'] = game_date
 
